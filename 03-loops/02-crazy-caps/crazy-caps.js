ce99bd83b615d87f9bc28fa943d887a00bddecfd
Sam Tok <sarinjiodtu@gmail.com>
2021-10-26 16:54:23 -0500

    03-loops first 3 exercises

--- 03-loops/02-crazy-caps/crazy-caps.js
+++ 03-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+
+function crazyCaps(word) {
+    let crazyString = "";
+    crazyString += word[0].toLowerCase();
+    for (var i = 1; i < word.length; i++) {
+        if(i % 2 !== 0) {
+            crazyString += word[i].toUpperCase();
+        }
+        else{
+            crazyString += word[i];
+        }
+    }
+    return crazyString;
+}
+
+const caps = crazyCaps('Alexander');
+console.log(caps)
--- 03-loops/03-bacteria-time/bacteria-time.js
+++ 03-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+
+function bacteriaTime(currentNum, targetNum) {
+    var time=0;
+    var alert = 'targetNum must be larger than currentNum';
+    if (targetNum < currentNum) {
+        return alert;
+    }
+    while(targetNum>currentNum){
+        
+            time += 20;
+            targetNum /= 2;
+        
+    }
+    return time;
+}
+
+const bacteria = bacteriaTime(1, 80);
+console.log(bacteria)

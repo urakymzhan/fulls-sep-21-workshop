4fe09188254df59bd982463e569826e9ddaa3fcc
Tima <kg13721@gmail.com>
2021-10-26 18:53:52 -0500

    completed some of the tasks

--- 01-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ 01-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,8 @@
-// YOUR CODE BELOW
+  // YOUR CODE BELOW
+  function isTruthy(arg) {
+    if (arg === false || arg === null || (typeof arg === 'undefined') || arg === 0 || arg == '') {
+        return false;
+    } else {
+        return true;
+    }
+}
--- 02-functions/03-tax-calculator/tax-calculator.js
+++ 02-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function taxCalculator(amount, state) {
+    let njTaxRate=0.06625;
+    let nyTaxRate=0.04
+    if (state == 'NY') {
+        return amount + (amount * nyTaxRate);
+    }else if(state == 'NJ'){
+        return amount + (amount * njTaxRate);
+    }
+}

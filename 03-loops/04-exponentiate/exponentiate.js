6e03a51c3ef5e86a2b3b811a82f641bab9c50320
Sam Tok <sarinjiodtu@gmail.com>
2021-10-26 20:27:41 -0500

    03-loops other exercises

--- 03-loops/04-exponentiate/exponentiate.js
+++ 03-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+function exponentiate(base, power) {
+    let expTemp=1;
+
+    if (power === 0) {
+        return 1;
+    }
+   do{
+           expTemp *= base;
+           power -= 1;
+    } while(power>=1);
+    return expTemp;
+}
+
+const exp = exponentiate(5, 2);
+console.log(exp)
--- 03-loops/05-my-slice/my-slice.js
+++ 03-loops/05-my-slice/my-slice.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+
+function mySlice(originalString, startIdx =0, endIdx = originalString.length) {
+    let tempString = originalString.substring(startIdx, endIdx);
+    return tempString;
+}
+
+const exp = mySlice('slice and dice',2);
+console.log(exp)
